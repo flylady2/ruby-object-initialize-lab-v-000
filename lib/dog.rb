23db6cbfae94271128aabfb0b6b43020fbdbeed1
Leslie Stevens <flylady@mail.utexas.edu>
2018-03-23 22:46:06 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,13 @@
 class Dog
-  def initialize(breed = "Mutt")
+  def initialize(name, breed = "Mutt")
+    @name = name
     @breed = breed
+  end
+  def name=(name)
+    @name = name
+  end
+  def name
+    @name
   end
   def breed=(breed)
     @breed = breed
